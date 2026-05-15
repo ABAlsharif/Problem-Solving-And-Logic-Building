b3f4d8c22cbd49e721f66778e140f7243586f644
ABAlsharif <abubakralshareef1@gmail.com>
2026-05-16 02:36:09 +0300

    Add files via upload

---
+++ Problem 1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+/* problem 1: Write Program Print Your Name On Screen*/
+
+
+void PrintName(string Name) {
+
+    cout << "My Name is : " << Name << endl;
+}
+
+int main()
+{
+    PrintName("Aboubakr");
+}
